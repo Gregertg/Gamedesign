5ab15a7a3c17ae5c80d7516199c3c3aa8a60f645
Gregertg <gregertg@stud.ntnu.no>
2022-10-09 19:12:48 +0200

    testing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello Hello Hello");
     }
 
     // Update is called once per frame
