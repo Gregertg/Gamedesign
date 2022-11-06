48ce5871c54334b2a3bbc715456be23cf1820d27
Gregertg <gregertg@stud.ntnu.no>
2022-11-06 16:45:32 +0100

    Start time fixed

--- Assets/Scripts/Tid.cs
+++ Assets/Scripts/Tid.cs
@@ -7,8 +7,8 @@ public class Tid : MonoBehaviour
 {
 
     public TMP_Text textTimer;
-    public float timer = 1320.0f; //how long since the timer "started"
- 
+    private float timer = 1320.0f; //how long since the timer "started"
+
     // Update is called once per frame
     void FixedUpdate()
     {
