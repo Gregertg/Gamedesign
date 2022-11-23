f71572c3a8164778c630ac1c1641431116a5c5f2
Gregertg <gregertg@stud.ntnu.no>
2022-11-23 23:55:04 +0100

    ANIMASJONEN FUNKERRRRRR

--- Assets/Scripts/ChangeSceneWithButton.cs
+++ Assets/Scripts/ChangeSceneWithButton.cs
@@ -5,11 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class ChangeSceneWithButton : MonoBehaviour
 {
+    public Animator transition;
+    //public float transitionTime = 30.0f;
+    bool click = false;
 
-
-    public void LoadScene(string sceneName){
+/*
+    public void LoadNextLevel(string sceneName){
         SceneManager.LoadScene(sceneName);
     }
+    void update(){
+        if(Input.GetMouseButtonDown(0)){
+            LoadNextLevel();
+        }
+    }
+*/
+    public void update(){
+        bool click = true;
+        print(click);
+        print(click + "yo");
+        if(click){
+            print(click + "yo34");
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex +1));
+            print(click + "yoyoyooyoy");
+        }
+    }
 
+
+
+    public IEnumerator LoadLevel(int levelIndex){
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(5);
+
+        SceneManager.LoadScene(levelIndex);
+    }
 }
+
+
 
