7605b7ee17861687492734e5d22c350525bf7ccc
Gregertg <gregertg@stud.ntnu.no>
2022-11-24 00:19:06 +0100

    button instructions -> game

--- Assets/Scripts/ChangeSceneWithButton.cs
+++ Assets/Scripts/ChangeSceneWithButton.cs
@@ -6,27 +6,12 @@ using UnityEngine.SceneManagement;
 public class ChangeSceneWithButton : MonoBehaviour
 {
     public Animator transition;
-    //public float transitionTime = 30.0f;
     bool click = false;
 
-/*
-    public void LoadNextLevel(string sceneName){
-        SceneManager.LoadScene(sceneName);
-    }
-    void update(){
-        if(Input.GetMouseButtonDown(0)){
-            LoadNextLevel();
-        }
-    }
-*/
     public void update(){
         bool click = true;
-        print(click);
-        print(click + "yo");
         if(click){
-            print(click + "yo34");
             StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex +1));
-            print(click + "yoyoyooyoy");
         }
     }
 
